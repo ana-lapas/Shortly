d4dffb1ec8eca6ab186ec797fd1357c5306a25e3
ana-lapas <anapaulalapas@gmail.com>
2023-03-01 21:26:28 -0400

    Fix: dump.sql

--- dump.sql
+++ dump.sql
@@ -28,7 +28,8 @@ CREATE TABLE public.sessions (
     id integer NOT NULL,
     "userId" integer NOT NULL,
     token text NOT NULL,
-    "isOnline" boolean DEFAULT true NOT NULL
+    "isOnline" boolean DEFAULT true NOT NULL,
+    "createdAt" timestamp without time zone DEFAULT now() NOT NULL
 );
 
 
@@ -144,55 +145,39 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 -- Data for Name: sessions; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.sessions VALUES (2, 1, '6bb637f0-c8b9-4851-87d4-5f9ca7e40ed1', true);
-INSERT INTO public.sessions VALUES (3, 1, '8cf3a085-2a57-4cc9-9c97-f3d7971e5ca5', true);
-INSERT INTO public.sessions VALUES (4, 1, '81784fca-21ce-43bb-a58a-6b5c58932834', true);
-INSERT INTO public.sessions VALUES (5, 1, 'c31d858a-ac9d-4ac5-aa02-44034434b676', true);
-INSERT INTO public.sessions VALUES (6, 1, 'c85b562e-baf7-4f4c-ac32-3f59b07beeff', true);
-INSERT INTO public.sessions VALUES (7, 1, '905c0a3d-4cf2-4bfe-a07e-1588a9b23fe8', true);
-INSERT INTO public.sessions VALUES (8, 1, 'b52ce319-56bd-4c97-a955-c08239117b6c', true);
-INSERT INTO public.sessions VALUES (9, 1, '8bd66930-3d2a-440c-9b6a-bdddec732602', true);
-INSERT INTO public.sessions VALUES (10, 2, '5b070432-100a-4a80-a5ed-6f8844a00898', true);
 
 
 --
 -- Data for Name: urls; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.urls VALUES (1, 'yVoQijH0', 'http://teste.com', 1, '2023-02-26 00:59:43.297246', 0);
-INSERT INTO public.urls VALUES (7, 'm_quHCwJ', 'http://teste2.com', 1, '2023-02-26 01:05:03.990365', 0);
-INSERT INTO public.urls VALUES (9, 'kZXaLSwk', 'http://google.com', 1, '2023-02-26 15:04:35.221331', 0);
-INSERT INTO public.urls VALUES (11, 'PLN_YANc', 'http://www.google.com', 2, '2023-02-26 16:15:51.573508', 1);
-INSERT INTO public.urls VALUES (12, 'Qm5FYXaY', 'https://www.instagram.com/', 2, '2023-02-26 16:17:05.938764', 0);
 
 
 --
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.users VALUES (1, 'João', '$2b$10$534/lKgyJb/Kt0c8sS8eG..n.EuAXDJrFAhI7zwSiW0swwvegyDui', 'joao@driven.com.br', '2023-02-25 23:38:48.129924');
-INSERT INTO public.users VALUES (2, 'Aninha', '$2b$10$LZez7Rq6EAENBbK3L3FW0uw3nRJzTSRBPUa0QcjxFEeQq8UimqQzu', 'ana@driven.com.br', '2023-02-26 16:15:12.473275');
 
 
 --
 -- Name: sessions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.sessions_id_seq', 10, true);
+SELECT pg_catalog.setval('public.sessions_id_seq', 1, false);
 
 
 --
 -- Name: urls_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.urls_id_seq', 12, true);
+SELECT pg_catalog.setval('public.urls_id_seq', 1, false);
 
 
 --
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 2, true);
+SELECT pg_catalog.setval('public.users_id_seq', 1, false);
 
 
 --
